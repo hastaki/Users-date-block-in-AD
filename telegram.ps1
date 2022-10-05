8e5bd790f134a43b87a0e03669be9effc6a6543b
hastaki <86711270+hastaki@users.noreply.github.com>
2022-10-05 12:39:01 +0300

    Add files via upload

---
+++ telegram.ps1
@@ -0,0 +1,25 @@
+﻿$token = "185216020327:AAF5sPvtRFVfc-Ndqf2JhBE1NAQae5XQ_nGGz_fqc"
+$Chat_Id = '-75121123142340568'
+$warnDays = (get-date)
+$2Day = (get-date).adddays(-1)
+$12=Get-ADUser -Filter * -Properties samaccountname,AccountExpirationDate | where AccountExpirationDate -lt $warnDays |where AccountExpirationDate -gt $2Day  |select samaccountname,AccountExpirationDate
+Foreach ($User in $12)
+{
+$1=($12.SamAccountName);
+$2=($12.AccountExpirationDate);
+
+
+}
+$2
+
+$1
+
+ if (($2 -lt $warnDays) -and ($2Day -lt $2) ){      
+$text = "У пользователя $($1) сегодня заблокировалась учетная запись"
+$URI = "https://api.telegram.org/bot" + $token + "/sendMessage?chat_id=" + $chat_id + "&text=" + $text
+$Request = Invoke-WebRequest -URI ($URI)  
+        }
+        else {
+     
+        }
+$12
